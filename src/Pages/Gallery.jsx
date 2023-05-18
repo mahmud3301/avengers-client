f21e8ac2095524070cd7340edc4d484a61bc89e7
Mahmudul Hasan Mahi <mahmudulhasan13189@gmail.com>
2023-05-18 19:34:46 +0600

    Gallery done

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,21 +1,58 @@
-
+import { useEffect, useState } from "react";
+import LazyLoad from "react-lazyload";
 
 const Gallery = () => {
+  const [galleryData, setGalleryData] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+  const [isBlur, setIsBlur] = useState(true);
+
+  useEffect(() => {
+    fetch("gallerydata.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setGalleryData(data);
+        setTimeout(() => {
+          setIsBlur(false);
+        }, 4000);
+      });
+  }, []);
+
+  const handleShowAll = () => {
+    setShowAll(true);
+  };
+
   return (
     <div id="gallery">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className="p-8 container">
+        <div>
+          <h2 className="text-3xl font-bold text-center mb-24 mt-11">
+            Our Top Avenger <span className="text-primary">Toys</span>
+          </h2>
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-4">
+            {galleryData?.slice(0, showAll ? 12 : 8).map((image) => (
+              <LazyLoad key={image.id} once>
+                <div className="relative">
+                  <img
+                    src={image.src}
+                    alt={image.caption}
+                    className={`w-full h-96 rounded p-4 ${isBlur ? "blur" : ""}`}
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300 bg-black bg-opacity-50 rounded">
+                    <p className="text-white text-lg font-bold">
+                      {image.caption}
+                    </p>
+                  </div>
+                </div>
+              </LazyLoad>
+            ))}
           </div>
+          {!showAll && (
+            <div className="text-center">
+              <span onClick={handleShowAll}>
+                <button className="btn btn-primary">See More</button>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
